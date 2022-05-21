e60a8bbabf5116276b432e308f64167d92380fba
Kihh <88188791+Kihh@users.noreply.github.com>
2022-05-21 15:29:06 +0800

    Update MFBL.sh

--- MFBL.sh
+++ MFBL.sh
@@ -21,19 +21,27 @@ ubuntu_check() {
         exit 1
     fi
 }
+cmenu() {
+    echo && echo -n -e "${yellow}* 按回车返回主菜单 *${none}" && read temp
+    menu
+}
 #正片开始
 menu(){
 clear
 echo "------------MFBL安装程序-----------------"
-echo "  1.一键安装java default + MCJAVA 1.16.5"
+echo "  1.一键安装java default + MC java 1.16.5"
 echo ""
-echo "  2.启动 MCJAVA 服务端"
+echo "  2.启动 MC Java 服务端"
 echo ""
-echo "  3.穷请点此，替换覆盖关闭正版验证文件"
+echo "  3.替换覆盖MC Java 关闭正版验证配置"
 echo ""
-echo "  4.退出脚本"
+echo "  4.一键安装MC Bedrock 1.18.33.02"
+echo ""
+echo "  5.启动 MC Bedrock 服务端"
+echo ""
+echo "  6.退出脚本"
 echo "----------------------------------------"
-echo "#如有需要自行挂screen运行本脚本，脚本已自动安装screen"
+echo "#如有需要自行挂screen运行本脚本，运行一键安装后会自动安装screen"
 
 read -e -p "请输入对应的数字：" num
 case $num in
@@ -41,26 +49,44 @@ case $num in
     clear
     ubuntu_check
     echo "开始安装java default + MCJAVA 1.16.5"
+    apt install -y bash curl sudo screen
     sudo apt install default-jdk -y
     wget https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar
     wget https://raw.githubusercontent.com/Kihh/MFBL/main/eula.txt
     wget https://raw.githubusercontent.com/Kihh/MFBL/main/olmode/server.properties
     echo "java default + MCJAVA 1.16.5 安装成功，请执行2开启服务端！"
-    menu
+    cmenu
 	;;
 	2)
     clear
     echo "正在启动Minecraft服务端，已自动同意EULA协议"
     sudo java -Xms1024m -Xmx1024m -jar server.jar nogui
     echo "服务端已关闭！"
-    menu
+    cmenu
 	;;
     3)
     wget https://raw.githubusercontent.com/Kihh/MFBL/main/falseolmode/server.properties
     echo "关闭正版验证成功！"
-    menu
+    cmenu
 	;;
-	4)
+    4)
+    clear
+    ubuntu_check
+    echo "开始安装MC Bedrock 1.18.33.02"
+    apt install -y bash curl sudo screen unzip
+    wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.18.33.02.zip
+    unzip bedrock-server-1.18.33.02.zip
+    echo "MC Bedrock 1.18.33.02安装成功，请执行5开启服务端！"
+    cmenu
+	;;
+    5)
+    clear
+    echo "正在启动Minecraft Bedrock服务端"
+    LD_LIBRARY_PATH=. ./bedrock_server
+    echo "服务端已关闭！"
+    cmenu
+	;;
+	6)
 	exit 0
 	;;
 	*)
@@ -68,13 +94,4 @@ case $num in
 esac
 }
 
-# 说明
-echo
-echo -e "$yellow此脚本适用于Ubuntu系统，最佳适用于Ubuntu 20+系统，非Ubuntu系统请重装后部署此脚本！按Ctrl + C退出MFBL$none"
-echo "----------------------------------------------------------------"
-read -rsp "$(echo -e "点按 $green Enter  $none 继续   或按 $red Ctrl + C $none 退出脚本.")" -d $'\n'
-echo
-#更新系统centos+ubuntu
-apt update
-apt install -y bash curl sudo screen
 menu
