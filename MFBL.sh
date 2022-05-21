f30b3019e0a0850b929b173b8591e4989a1814f4
Kihh <88188791+Kihh@users.noreply.github.com>
2022-05-21 14:45:29 +0800

    Update MFBL.sh

--- MFBL.sh
+++ MFBL.sh
@@ -33,7 +33,7 @@ echo "  3.穷请点此，替换覆盖关闭正版验证文件"
 echo ""
 echo "  4.退出脚本"
 echo "----------------------------------------"
-echo "$yellow#如有需要自行挂screen运行本脚本，脚本已自动安装screen$none"
+echo "$red #如有需要自行挂screen运行本脚本，脚本已自动安装screen $none"
 
 read -e -p "请输入对应的数字：" num
 case $num in
